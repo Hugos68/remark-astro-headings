ffa4bae225197678a3e24448a011598e1c60e774
Hugos68 <hugokorteapple@gmail.com>
2024-03-07 13:10:05 +0100

    Just better

---
+++ .changeset/thirty-zoos-reflect.md
@@ -0,0 +1,5 @@
+---
+"remark-astro-headings": patch
+---
+
+Added createSlugger for custom slugging, improved readability
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,86 @@
-import { generateHeadings } from './generate-headings.js';
-import type { Transformer } from 'unified';
+/// <reference types="mdast-util-mdxjs-esm" />
+import GithubSlugger from 'github-slugger';
+import { visit } from 'unist-util-visit';
+import type { Root } from 'mdast';
+import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
+import { fromMarkdown } from 'mdast-util-from-markdown';
+import { Transformer } from 'unified';
 
-export type Config = {
+const IMPORT_PATH_REGEX = /from\s+['"]([^'"]+)['"]/;
+
+type AstroHeadingsConfig = {
 	minDepth?: number;
 	maxDepth?: number;
+	createSlugger?: () => { slug: (content: string) => string };
 };
 
-export function remarkAstroHeadings(config: Config = {}): Transformer {
-	return function (root, VFile) {
-		const filePath = VFile.history[0];
-		const headings = generateHeadings(root, {
-			minDepth: config.minDepth,
-			maxDepth: config.maxDepth,
-			filePath,
-		});
-		if (VFile.data.astro) {
-			// @ts-ignore - If `astro` is present then frontmatter is also present
-			VFile.data.astro.frontmatter.headings = headings;
+function remarkAstroHeadings(config: AstroHeadingsConfig = {}): Transformer<Root> {
+	const configWithDefaults = {
+		minDepth: 2,
+		maxDepth: 6,
+		createSlugger: () => new GithubSlugger(),
+		...config,
+	};
+	return generateHeadings(configWithDefaults);
+}
+
+function generateHeadings(config: Required<AstroHeadingsConfig>): Transformer<Root> {
+	return (root, VFile) => {
+		if (!VFile.data.astro) {
+			console.warn(`[remark-astro-headings] No astro data found in VFile: ${VFile.path}`);
+			return;
 		}
+		const slugger = config.createSlugger();
+		const dir = join(VFile.path, '..');
+		const headings = visitDeeply(root, { dir, headings: [], slugger, ...config });
+		// @ts-ignore - If `astro` is present then frontmatter is also present
+		VFile.data.astro.frontmatter.headings = headings;
 	};
 }
+
+type VisitRecursivelyConfig = {
+	dir: string;
+	slugger: { slug: (content: string) => string };
+	headings: { content: string; slug: string; depth: number }[];
+	minDepth: number;
+	maxDepth: number;
+};
+
+function visitDeeply(root: Root, config: VisitRecursivelyConfig) {
+	visit(root, ['heading', 'mdxjsEsm'], (child) => {
+		switch (child.type) {
+			case 'heading': {
+				const content = 'value' in child.children[0] && child.children[0].value;
+				if (!content) {
+					break;
+				}
+				const depth = child.depth;
+				if (depth < config.minDepth || depth > config.maxDepth) {
+					break;
+				}
+				const slug = config.slugger.slug(content);
+				config.headings.push({
+					content,
+					slug,
+					depth,
+				});
+				break;
+			}
+			case 'mdxjsEsm': {
+				const match = child.value.match(IMPORT_PATH_REGEX);
+				if (!match) {
+					break;
+				}
+				const importPath = match[1];
+				const file = readFileSync(join(config.dir, importPath), 'utf-8');
+				const root = fromMarkdown(file);
+				visitDeeply(root, config);
+				break;
+			}
+		}
+	});
+	return config.headings;
+}
+
+export default remarkAstroHeadings;
